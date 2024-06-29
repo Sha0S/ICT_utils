7a645e0a3a1b65e3d47bc9327d80fdd0eccaf816
Bernát Süli <suli.bernat@gmail.com>
2024-06-29 13:50:01 +0200

    traceability-client implemented

--- crates/traceability-client/src/main.rs
+++ crates/traceability-client/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::path::PathBuf;
+
+use ICT_config::*;
+
+static CONFIG: &str = "config.ini";
+
+/*
+1) At the start of the ICT test, check if the board is "testable".
+    Params: START {Main DMC of the board} {Number of noards on the panel}
+    Return message from the traceability-server:
+        a) Panel is golden sample: "GS"
+        b) Panel is testable: "OK"
+        c) Panel is not testable: "NK"
+        d) System error: "ER: {error message}"
+
+2) At the end of the ICT test, send the paths of the logs to be processed.
+    Params: END {list of paths for the logs}
+    Return message from the traceability-server:
+        a) Upload is succesfull: "OK"
+        b) Upload failed: "ER: {error message}"
+*/
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = match Config::read(PathBuf::from(CONFIG)) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("{e}");
+            std::process::exit(0)
+        }
+    };
+
+    if config.get_MES_server().is_empty() {
+        println!("ER: Configuartion is missing the adress of the MES server!")
+    }
+
+    let mut stream = TcpStream::connect(config.get_MES_server())?;
+
+    let tokens: Vec<&str> = args.iter().skip(1).map(|f| f.trim() ).collect(); 
+    let message = tokens.join("|");
+
+    stream.write_all(message.as_bytes())?;
+
+    let mut buf = [0;1024];
+    if stream.read(&mut buf).is_ok() {
+        let message = String::from_utf8_lossy(&buf).trim_end_matches('\0').to_string();
+        println!("{message}");
+    } else {
+        println!("ER: Failed to read response from server!");
+    }
+
+    Ok(())
 }
