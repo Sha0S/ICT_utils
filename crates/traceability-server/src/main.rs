8ee30b5c49ee76be921c4cc53a31ffa0a4568c8e
Bernát Süli <suli.bernat@gmail.com>
2024-07-11 13:01:19 +0200

    hide console in release

--- crates/traceability-server/src/main.rs
+++ crates/traceability-server/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(non_snake_case)]
 #![allow(clippy::collapsible_match)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::{
     sync::{mpsc, Arc, Mutex},
