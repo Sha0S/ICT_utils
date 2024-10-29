6bd5c7cc3a2996f17adf561609d78249c50edcfd
Bernát Süli <155308506+Sha0S@users.noreply.github.com>
2024-10-29 04:51:11 +0100

    auth: documentation

--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -1,10 +1,14 @@
+//! Mudule containing authentication functions
+
 #![allow(non_snake_case)]
 
 use std::{fs, io::Write};
 use pwhash::bcrypt;
 
+/// Default filename for the file containing local userdata
 static USER_LIST: &str = "users";
 
+/// Authentication levels, based on which users get privileges
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub enum UserLevel {
     Admin = 2,
@@ -42,6 +46,11 @@ impl UserLevel {
     }
 }
 
+/// User struct 
+///
+/// name: name of the user, public
+/// level: authentication level of the user, public
+/// hash: hashed password of the user. Uses salt and pepper. 
 #[derive(Debug, Clone)]
 pub struct User {
     pub name: String,
@@ -50,6 +59,7 @@ pub struct User {
 }
 
 impl User {
+    /// Creates new user, with no password
     pub fn new(name: String, level: UserLevel) -> Self {
         Self {
             name,
@@ -58,27 +68,30 @@ impl User {
         }
     }
 
+    /// Creates hash for the given password
     pub fn create_hash(&mut self, pass: &str) {
         self.hash = bcrypt::hash(self.level.pepper(pass)).unwrap();
     }
 
+    /// Checks if the given password matches the stored hash
     pub fn check_pw(&self, pass: &str) -> bool {
         bcrypt::verify(self.level.pepper(pass), &self.hash)
     }
 }
 
+/// Load the user data from the default USER_LIST file.
 pub fn load_user_list() -> Vec<User> {
     let mut ret = Vec::new();
 
     if let Ok(fileb) = fs::read_to_string(USER_LIST) {
         let lines: Vec<String> = fileb
             .lines()
-            .filter(|f| !f.starts_with('!') && !f.is_empty())
+            .filter(|f| !f.starts_with('!') && !f.is_empty()) // Lines starting with '!' are considered comments
             .map(|f| f.to_owned())
             .collect();
 
         for line in lines {
-            let tokens: Vec<&str> = line.split('|').collect();
+            let tokens: Vec<&str> = line.split('|').collect(); // The fields are seperated with a '|' character
             if tokens.len() >= 3 {
                 ret.push(User {
                     name: tokens[0].to_string(),
@@ -92,6 +105,7 @@ pub fn load_user_list() -> Vec<User> {
     ret
 }
 
+/// Export the given user data to the default USER_LIST file.
 pub fn save_user_list(users: &[User]) {
     if let Ok(mut file) = fs::File::create(USER_LIST) {
         for user in users {
